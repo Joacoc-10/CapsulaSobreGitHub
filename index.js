748efae9caa8755c1e5ddb971d210ecba24f55d1
Franco Gauna <francorgauna@gmail.com>
2025-04-11 18:42:44 -0300

    fix/ Minor changes

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-
 function laCajaDePandora(numero) {
+  if (!Number.isInteger(numero)) return 'Debe ingresar un numero entero';
   if (numero % 2 == 0) return numero.toString(2);
   if (numero % 2 == 1) return numero.toString(16);
 }
 
-function joaquinCurbelo () {
-  return {  
+function Joaco() {
+  return {
     nombre: 'Joaquin Curbelo',
     edad: 27,
     nacionalidad: 'Uruguayo',
-  }
+  };
 }
 
 function Franco() {
   return {
-    nombre: 'Franco',
+    nombre: 'Franco Gauna',
     edad: 23,
-    nacionalidad: 'argentina',
-      }
-    }
+    nacionalidad: 'Argentina',
+  };
+}
 
 function Tobias() {
   return {
-    nombre: "Tobias",
+    nombre: 'Tobias Gonzalez',
     edad: 26,
-    nacionalidad: "Argentina",
-  }
+    nacionalidad: 'Argentina',
+  };
 }
