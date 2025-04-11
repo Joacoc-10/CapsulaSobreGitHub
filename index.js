3f91d88d95365205c4a67ed3f788bdada7ae9875
Joaco Curbelo <joacoc-10@hotmail.com>
2025-04-11 22:48:40 +0200

    Ejercicio 8

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-function laCajaDePandora(numero) {
-  // proximamente escribiremos codigo aqui
-}
-let hola;
 
-let HolaMundo;
+function laCajaDePandora(numero) {
+  if (numero % 2 == 0) return numero.toString(2);
+  if (numero % 2 == 1) return numero.toString(16);
+}
+
+console.log(laCajaDePandora(234));
